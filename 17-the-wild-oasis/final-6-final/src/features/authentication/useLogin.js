04f94a8d07abf63eb0ed3b19a06bb3c4ca528682
Jonas Schmedtmann <hello@jonas.io>
2023-05-19 13:04:47 +0100

    Fix small bug in auth

--- 17-the-wild-oasis/final-6-final/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/final-6-final/src/features/authentication/useLogin.js
@@ -1,7 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { login as loginApi } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
 
 export function useLogin() {
   const queryClient = useQueryClient();
@@ -10,12 +10,12 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard", { replace: true });
+      queryClient.setQueryData(['user'], user.user);
+      navigate('/dashboard', { replace: true });
     },
     onError: (err) => {
-      console.log("ERROR", err);
-      toast.error("Provided email or password are incorrect");
+      console.log('ERROR', err);
+      toast.error('Provided email or password are incorrect');
     },
   });
 
