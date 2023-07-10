00573c2684c1f8ea3805e23ba5dc8675db4f0259
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-10 14:11:33 +0330

    JS review

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,36 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// ? Destructuring
+
+const book = getBook(1);
+book;
+
+// const title = book.title;
+// const author = book.author
+
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+  book;
+book;
+
+console.log(author, title, genres);
+
+// const primaryGenre =  genres[0]
+// const secondaryGenre =  genres[1]
+
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+const newGenres = [...genres, "epic fantasy"];
+newGenres;
+
+const updateBook = {
+  ...book,
+  //! Adding a new property
+  moviePublicationDate: "2001-12-19",
+  //! Overwriting an existing property
+  pages: 1210,
+};
+updateBook;
---
+++ 03-pizza-menu/starter/pizza-menu/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <h1>Hello React/h1>;
+}
+
+export default App;
