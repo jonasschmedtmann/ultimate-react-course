c44b9ef081beab11d8562489c94a000a04980c97
kjroelke <kj.roelke@gmail.com>
2023-08-21 09:33:10 -0500

    add FormRowVertical into UI folders

---
+++ 17-the-wild-oasis/final-4-after-advanced-patterns/src/ui/FormRowVertical.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const StyledFormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  padding: 1.2rem 0;
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+function FormRowVertical({ label, error, children }) {
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
+}
+
+export default FormRowVertical;
