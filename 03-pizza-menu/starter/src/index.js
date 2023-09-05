760dedbd4ac0bdedda2061d5919a17c8e5454b20
Xuan <xuancbblin@gmail.com>
2023-09-05 14:55:50 +0800

    ch5: 38. Creating More Components

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -49,12 +49,32 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Fast React Pizza Co.</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>;
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
