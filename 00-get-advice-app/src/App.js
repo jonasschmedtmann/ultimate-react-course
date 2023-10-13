9a95288e92786a2ac58f1a2392d558fc994dfe65
CC <cecilia.streamlineai@gmail.com>
2023-10-13 11:33:50 -0700

    complete get advice app project

---
+++ 00-get-advice-app/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice.
+    </p>
+  );
+}
