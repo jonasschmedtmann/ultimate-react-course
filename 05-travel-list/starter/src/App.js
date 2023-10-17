6baff5e593a4915ab50fecce99798f294cb1904f
Yash Mehta <yashmehta001@yahoo.in>
2023-10-17 21:41:48 +0530

    react states

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./index";
 
 const initialItems = [
@@ -22,21 +23,41 @@ function Logo() {
 }
 
 function Form() {
-  function handleSubmit(e){
-    e.preventDefault()
-    console.log(e)
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!description) return
+    const newItem = {
+      description, quantity, packed: false, id: Date.now()
+    }
+    console.log(newItem)
+    setDescription('')
+    setQuantity(1)
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do we need for our trip</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          setQuantity(+e.target.value);
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Items"></input>
+      <input
+        type="text"
+        placeholder="Items"
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
       <button>Add</button>
     </form>
   );
