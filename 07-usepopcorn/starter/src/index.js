b926bc137828defd1fe00c9cecd45d1864f34c1c
Yash <yash.mehta@jklgroup.in>
2023-10-19 17:27:33 +0530

    react day 6

--- 07-usepopcorn/starter/src/index.js
+++ 07-usepopcorn/starter/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+// import './index.css';
+// import App from './App';
+import StarRating from './starRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5}/>
+    <StarRating maxRating={10}/>
+    <StarRating />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
