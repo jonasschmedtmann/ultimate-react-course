456c472affc51c91c16ac9a6a62792267e88e2a6
Yash Mehta <yashmehta001@yahoo.in>
2023-10-19 21:42:22 +0530

    init

--- 08-how-react-works/starter/src/App.js
+++ 08-how-react-works/starter/src/App.js
@@ -39,7 +39,7 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent item={content.at(activeTab)}  key={content.at(activeTab).summary}/>
       ) : (
         <DifferentContent />
       )}
