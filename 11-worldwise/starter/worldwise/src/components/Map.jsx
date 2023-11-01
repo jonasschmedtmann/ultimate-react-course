c40facf5f82dc6057f0b954354ccb3db7827f39a
Yash Mehta <yashmehta001@yahoo.in>
2023-11-01 23:18:54 +0530

    added cities pin

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -2,9 +2,11 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
+import { useCities } from "../../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   // eslint-disable-next-line no-unused-vars
   const [mapPosition, setMapPosition] = useState([40, 0]);
   // eslint-disable-next-line no-unused-vars
@@ -23,11 +25,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
