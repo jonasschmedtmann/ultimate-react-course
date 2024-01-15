a94e9049566933a020a35dde2d8fedfe8c18bffb
Sivasish Praharaj <sivasish48@gmail.com>
2024-01-15 10:42:50 +0530

    Issue #42 Fixed all the incorrect guest ID mapping in createBookings function from using cabin ID

--- 17-the-wild-oasis/final-6-final/src/data/Uploader.jsx
+++ 17-the-wild-oasis/final-6-final/src/data/Uploader.jsx
@@ -46,7 +46,7 @@ async function createBookings() {
     .from("guests")
     .select("id")
     .order("id");
-  const allGuestIds = guestsIds.map((cabin) => cabin.id);
+  const allGuestIds = guestsIds.map((guest) => guest.id);
   const { data: cabinsIds } = await supabase
     .from("cabins")
     .select("id")
