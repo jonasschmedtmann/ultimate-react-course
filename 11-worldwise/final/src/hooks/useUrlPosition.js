f0a2e2c0c77c7f24fca7de960b2b7a7e1f44ec24
Sparsh Gupta <47265752+sprsgupta@users.noreply.github.com>
2024-03-10 09:23:30 +0530

    Delete 11-worldwise/final directory

--- 11-worldwise/final/src/hooks/useUrlPosition.js
+++
@@ -1,9 +0,0 @@
-import { useSearchParams } from "react-router-dom";
-
-export function useUrlPosition() {
-  const [searchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-
-  return [lat, lng];
-}
