487ff88a7d1f63d5b96e3e0229dd53233105ea40
saruit0105 <saruits2@gmail.com>
2024-07-26 22:18:23 -0400

    pizza-menu

--- proj/pizza-menu/src/App.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return <h1> Hello Reac</h1>;
-}
-
-export default App;
--- proj/pizza-menu/src/index.js
+++ proj/pizza-menu/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function App() {
+  return <h1> Hello React!</h1>;
+}
+
+//React v18 - Strict mode renders components twice to find bugs and find outdated parts of the API
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +13,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//React before 18
+// React.render(<App />);
