5c38ad0e9f5067d4a486e8ee5d7bed36268fbbb8
Jonas Schmedtmann <hello@jonas.io>
2024-11-08 15:36:52 +0000

    Update react-fast-pizza API URL

--- 16-fast-react-pizza/final-1-after-tailwind/src/services/apiRestaurant.js
+++ 16-fast-react-pizza/final-1-after-tailwind/src/services/apiRestaurant.js
@@ -1,4 +1,4 @@
-const API_URL = "https://react-fast-pizza-api.onrender.com/api";
+const API_URL = 'https://react-fast-pizza-api.jonas.io/api';
 
 export async function getMenu() {
   const res = await fetch(`${API_URL}/menu`);
